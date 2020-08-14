f412f0cc237192897e090559b78b46877648cf32
stephenajulu <alunje73@gmail.com>
2020-08-14 19:20:42 +0300

    First commit

---
+++ gridsome.config.js
@@ -0,0 +1,190 @@
+// This is where project configuration and plugin options are located. 
+// Learn more: https://gridsome.org/docs/config
+
+// Changes here require a server restart.
+// To restart press CTRL + C in terminal and run `gridsome develop`
+
+module.exports = {
+  siteName: 'TechWit Ke',
+  siteDescription: 'Your Source of Quality Tech, Cybersecurity,  Content',
+  siteUrl: 'https://techwit3.netlify.app',
+  titleTemplate: '%s | TechWit Ke',
+  icon: 'src/favicon.png',
+  plugins: [
+		{
+      use: '@gridsome/plugin-sitemap',
+      options: {
+        exclude: ['/exclude-me'],
+        config: {
+          '/*': {
+            changefreq: 'weekly',
+            priority: 0.5,
+          }
+        }
+      }
+    },
+	{
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: 'UA-XXXXXXXXX-X'
+      }
+    },
+	{
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    },
+	{
+      use: 'gridsome-plugin-feed',
+      options: {
+        // Required: array of `GraphQL` type names you wish to include
+        contentTypes: ['BlogPost', 'NewsPost','Post','Posts','Blog'],
+        // Optional: any properties you wish to set for `Feed()` constructor
+        // See https://www.npmjs.com/package/feed#example for available properties
+        feedOptions: {
+          title: 'TechWit Ke',
+          description: 'TechWit Ke, Your Trusted Source of Quality Tech Content For All.',
+		   link: "https://techwit3.netlify.app"
+        },
+        // === All options after this point show their default values ===
+        // Optional; opt into which feeds you wish to generate, and set their output path
+        rss: {
+          enabled: true,
+          output: '/feed.xml'
+        },
+        atom: {
+          enabled: true,
+          output: '/feed.atom'
+        },
+        json: {
+          enabled: true,
+          output: '/feed.json'
+        },
+        // Optional: the maximum number of items to include in your feed
+        maxItems: 50,
+        // Optional: an array of properties passed to `Feed.addItem()` that will be parsed for
+        // URLs in HTML (ensures that URLs are full `http` URLs rather than site-relative).
+        // To disable this functionality, set to `null`.
+        htmlFields: ['description', 'content'],
+        // Optional: if you wish to enforce trailing slashes for site URLs
+        enforceTrailingSlashes: false,
+        // Optional: a method that accepts a node and returns true (include) or false (exclude)
+        // Example: only past-dated nodes: `filterNodes: (node) => node.date <= new Date()`
+        filterNodes: (node) => true,
+        // Optional: a method that accepts a node and returns an object for `Feed.addItem()`
+        // See https://www.npmjs.com/package/feed#example for available properties
+        // NOTE: `date` field MUST be a Javascript `Date` object
+        nodeToFeedItem: (node) => ({
+          title: node.title,
+          date: node.date || node.fields.date,
+          content: node.content
+        })
+      }
+    },
+    {
+      use: 'gridsome-plugin-tailwindcss',
+      options: {
+        tailwindConfig: './tailwind.config.js',
+        purgeConfig: {
+          whitelist: ['svg-inline--fa', 'table', 'table-striped', 'table-bordered', 'table-hover', 'table-sm'],
+          whitelistPatterns: [/fa-$/, /blockquote$/, /code$/, /pre$/, /table$/, /table-$/, /vueperslide$/, /vueperslide-$/]
+        },
+        presetEnvConfig: {},
+        shouldPurge: false,
+        shouldImport: true,
+        shouldTimeTravel: true,
+        shouldPurgeUnusedKeyframes: true,
+      }
+    },
+    {
+      use: 'gridsome-source-static-meta',
+      options: {
+        path: 'content/site/*.json'
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Author',
+        path: './content/author/*.md'
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'Blog',
+        path: './content/blog/**/*.md',
+        refs: {
+          author: 'Author',
+          tags: {
+            typeName: 'Tag',
+            create: true
+          },
+          category: {
+            typeName: 'Category',
+            create: true
+          }
+        }
+      }
+    },
+    {
+      use: '@gridsome/source-filesystem',
+      options: {
+        typeName: 'CustomPage',
+        path: './content/pages/*.md'
+      }
+    },
+    {
+      use: 'gridsome-plugin-flexsearch',
+      options: {
+        searchFields: ['title', 'content'],
+        collections: [{
+          typeName: 'Blog',
+          indexName: 'Blog',
+          fields: ['title', 'category', 'excerpt', 'content']
+        }]
+      }
+    }
+  ],
+  transformers: {
+    remark: {
+      plugins: [
+        'remark-autolink-headings',
+        'remark-attr',
+        ['gridsome-plugin-remark-prismjs-all', {
+          noInlineHighlight: false,
+          showLineNumbers: false,
+        }],
+        require('./packages/gridsome-plugin-remark-figure')
+      ],
+      
+      processImages: false
+      
+    }
+  },
+  templates: {
+    Blog: [{
+      path: '/posts/:title'
+    }],
+    CustomPage: [{
+      path: '/:title',
+      component: '~/templates/CustomPage.vue'
+    }],
+    Category: [{
+      path: '/category/:title',
+      component: '~/templates/Category.vue'
+    }],
+    Author: [{
+      path: '/author/:name',
+      component: '~/templates/Author.vue'
+    }],
+    Tag: [{
+      path: '/tags/:title',
+      component: '~/templates/Tag.vue'
+    }]
+  },
+  chainWebpack: config => {
+      config.resolve.alias.set('@pageImage', '@/assets/images');
+  }
+}
