f96a0088c2a39f2d156832bdba1e23d3c512e56a
Stephen Ajulu <alunje73@gmail.com>
2020-08-15 12:47:54 +0300

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -35,53 +35,7 @@ module.exports = {
         publicPath: `/admin`
       }
     },
-	{
-      use: 'gridsome-plugin-feed',
-      options: {
-        // Required: array of `GraphQL` type names you wish to include
-        contentTypes: ['BlogPost', 'NewsPost','Post','Posts','Blog'],
-        // Optional: any properties you wish to set for `Feed()` constructor
-        // See https://www.npmjs.com/package/feed#example for available properties
-        feedOptions: {
-          title: 'TechWit Ke',
-          description: 'TechWit Ke, Your Trusted Source of Quality Tech Content For All.',
-		   link: "https://techwit3.netlify.app"
-        },
-        // === All options after this point show their default values ===
-        // Optional; opt into which feeds you wish to generate, and set their output path
-        rss: {
-          enabled: true,
-          output: '/feed.xml'
-        },
-        atom: {
-          enabled: true,
-          output: '/feed.atom'
-        },
-        json: {
-          enabled: true,
-          output: '/feed.json'
-        },
-        // Optional: the maximum number of items to include in your feed
-        maxItems: 50,
-        // Optional: an array of properties passed to `Feed.addItem()` that will be parsed for
-        // URLs in HTML (ensures that URLs are full `http` URLs rather than site-relative).
-        // To disable this functionality, set to `null`.
-        htmlFields: ['description', 'content'],
-        // Optional: if you wish to enforce trailing slashes for site URLs
-        enforceTrailingSlashes: false,
-        // Optional: a method that accepts a node and returns true (include) or false (exclude)
-        // Example: only past-dated nodes: `filterNodes: (node) => node.date <= new Date()`
-        filterNodes: (node) => true,
-        // Optional: a method that accepts a node and returns an object for `Feed.addItem()`
-        // See https://www.npmjs.com/package/feed#example for available properties
-        // NOTE: `date` field MUST be a Javascript `Date` object
-        nodeToFeedItem: (node) => ({
-          title: node.title,
-          date: node.date || node.fields.date,
-          content: node.content
-        })
-      }
-    },
+	// Gridsome Feed Was Here but caused some issues
     {
       use: 'gridsome-plugin-tailwindcss',
       options: {
